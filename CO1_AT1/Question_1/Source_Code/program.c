7377a2451e576f55b4e4cacda79931cbc05a0b50
Muni Chandini <muni.chandini29@gmail.com>
2026-07-19 04:39:20 +0530

    Update program.c

--- CO1_AT1/Question_1/Source_Code/program.c
+++ CO1_AT1/Question_1/Source_Code/program.c
@@ -1,1 +1,34 @@
-heloo
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n;
+    int a = 4, b = 2;
+
+    printf("Enter transaction size (n): ");
+    scanf("%d", &n);
+
+    double logValue = log(a) / log(b);
+
+    printf("\nMaster Theorem Analysis\n");
+    printf("-----------------------\n");
+    printf("Recurrence : T(n) = 4T(n/2) + n^2\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    printf("f(n) = n^2\n");
+    printf("log_b(a) = %.0lf\n", logValue);
+
+    if (logValue == 2)
+    {
+        printf("\nCase 2 of Master Theorem\n");
+        printf("Time Complexity = Theta(n^2 log n)\n");
+    }
+
+
+    printf("\nAnalysis:\n");
+    printf("The divide-and-conquer algorithm efficiently processes\n");
+    printf("large banking transactions with complexity Theta(n^2 log n).\n");
+
+    return 0;
+}
