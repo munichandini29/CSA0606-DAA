17f1444c04758951fe6f624299d425df7b416764
Muni Chandini <muni.chandini29@gmail.com>
2026-07-19 05:01:37 +0530

    Update program.c

--- CO1_AT1/Question_2/Source_Code/program.c
+++ CO1_AT1/Question_2/Source_Code/program.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+    int n;
+    int a = 2, b = 2;
+
+    printf("Enter audio input size (n): ");
+    scanf("%d", &n);
+
+    double logValue = log(a) / log(b);
+
+    printf("\nMaster Theorem Analysis\n");
+    printf("-----------------------\n");
+    printf("Recurrence : T(n) = 2T(n/2) + sqrt(n)\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    printf("f(n) = sqrt(n)\n");
+    printf("log_b(a) = %.0lf\n", logValue);
+
+    printf("\nCase 1 of Master Theorem\n");
+    printf("Time Complexity = Theta(n)\n");
+
+    printf("\nAnalysis:\n");
+    printf("The speech recognition system processes\n");
+    printf("large audio inputs with linear time complexity.\n");
+
+    return 0;
+}
