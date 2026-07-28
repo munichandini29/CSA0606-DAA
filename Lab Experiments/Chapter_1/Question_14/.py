14e8f77f26ada281378fbb39c11d38660b2db364
Muni Chandini <muni.chandini29@gmail.com>
2026-07-28 21:36:37 +0530

    Update .py

--- Lab Experiments/Chapter_1/Question_14/.py
+++ Lab Experiments/Chapter_1/Question_14/.py
@@ -1,1 +1,18 @@
+# Exponentiation using Divide and Conquer
 
+def power(a, n):
+    if n == 0:
+        return 1
+
+    half = power(a, n // 2)
+
+    if n % 2 == 0:
+        return half * half
+    else:
+        return a * half * half
+
+
+a = int(input("Enter base: "))
+n = int(input("Enter exponent: "))
+
+print("Result =", power(a, n))
