583dd1a0b3cf27cb4ac98ee588d3be2dd9be3758
Muni Chandini <muni.chandini29@gmail.com>
2026-07-28 11:52:50 +0530

    Update .py

--- Lab Experiments/Chapter_1/Question_4/.py
+++ Lab Experiments/Chapter_1/Question_4/.py
@@ -1,1 +1,19 @@
+# Fibonacci Series using Iterative and Recursive Methods
 
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+n = int(input("Enter number of terms: "))
+
+# Iterative Method
+print("Iterative:")
+a, b = 0, 1
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
+
+print("\nRecursive:")
+for i in range(n):
+    print(fibonacci(i), end=" ")
