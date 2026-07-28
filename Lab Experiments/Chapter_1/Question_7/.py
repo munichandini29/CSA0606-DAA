558d72a0d42ad8c883461050c8f90cfb863c6801
Muni Chandini <muni.chandini29@gmail.com>
2026-07-28 18:23:04 +0530

    Implement Quick Sort algorithm in Python

--- Lab Experiments/Chapter_1/Question_7/.py
+++ Lab Experiments/Chapter_1/Question_7/.py
@@ -1,1 +1,29 @@
+# Quick Sort
 
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[-1]
+    left = []
+    right = []
+
+    for i in arr[:-1]:
+        if i <= pivot:
+            left.append(i)
+        else:
+            right.append(i)
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+n = int(input("Enter number of elements: "))
+
+arr = []
+
+print("Enter the elements:")
+for i in range(n):
+    arr.append(int(input()))
+
+sorted_arr = quick_sort(arr)
+
+print("Sorted array:", sorted_arr)
