c98007e44c39b1c09bdbfc1beda74551a33d3497
Muni Chandini <muni.chandini29@gmail.com>
2026-07-28 18:29:26 +0530

    Update .py

--- Lab Experiments/Chapter_1/Question_8/.py
+++ Lab Experiments/Chapter_1/Question_8/.py
@@ -1,1 +1,15 @@
+# Tower of Hanoi
 
+def hanoi(n, source, auxiliary, destination):
+    if n == 1:
+        print("Move disk 1 from", source, "to", destination)
+        return
+
+    hanoi(n - 1, source, destination, auxiliary)
+    print("Move disk", n, "from", source, "to", destination)
+    hanoi(n - 1, auxiliary, source, destination)
+
+n = int(input("Enter number of disks: "))
+
+print("Steps to solve Tower of Hanoi:")
+hanoi(n, 'A', 'B', 'C')
