016829c1d650ee2274702096d802ff5fed89533e
Muni Chandini <muni.chandini29@gmail.com>
2026-07-28 10:56:36 +0530

    Update .py

--- Lab Experiments/Chapter_1/Question_3/.py
+++ Lab Experiments/Chapter_1/Question_3/.py
@@ -1,1 +1,16 @@
+# Factorial using Iterative and Recursive Methods
 
+def recursive_fact(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * recursive_fact(n - 1)
+
+n = int(input("Enter a number: "))
+
+# Iterative
+fact = 1
+for i in range(1, n + 1):
+    fact *= i
+
+print("Iterative Factorial =", fact)
+print("Recursive Factorial =", recursive_fact(n))
