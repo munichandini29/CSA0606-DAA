67c79f509868b96d9d534535231394c9fc13ef62
Muni Chandini <muni.chandini29@gmail.com>
2026-07-30 16:40:12 +0530

    Update .py

--- Lab Experiments/Chapter_2/Selection Sort/Question_1/.py
+++ Lab Experiments/Chapter_2/Selection Sort/Question_1/.py
@@ -1,1 +1,17 @@
+# Selection Sort - Top K Scores
 
+scores = list(map(int, input("Enter scores: ").split()))
+k = int(input("Enter Top K value: "))
+
+n = len(scores)
+
+for i in range(min(k, n)):
+    max_index = i
+
+    for j in range(i + 1, n):
+        if scores[j] > scores[max_index]:
+            max_index = j
+
+    scores[i], scores[max_index] = scores[max_index], scores[i]
+
+print("Top", k, "Scores:", scores[:k])
